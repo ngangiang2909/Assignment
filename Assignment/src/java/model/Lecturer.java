68b5f6dd9085655d536c32e1acf51a394fdbd5e6
ngangiang2909 <ngangiang2909@gmail.com>
2022-06-28 07:49:35 +0700

    update model

---
+++ Assignment/src/java/model/Lecturer.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author asus
+ */
+public class Lecturer {
+    private int lid;
+    private String lmember;
+    private String lname;
+    private boolean lgender;
+
+    public int getLid() {
+        return lid;
+    }
+
+    public void setLid(int lid) {
+        this.lid = lid;
+    }
+
+    public String getLmember() {
+        return lmember;
+    }
+
+    public void setLmember(String lmember) {
+        this.lmember = lmember;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public boolean isLgender() {
+        return lgender;
+    }
+
+    public void setLgender(boolean lgender) {
+        this.lgender = lgender;
+    }
+    
+}
---
+++ Assignment/src/java/model/Subject.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author asus
+ */
+public class Subject {
+    private int subid;
+    private String subcode;
+    private String subname;
+
+    public int getSubid() {
+        return subid;
+    }
+
+    public void setSubid(int subid) {
+        this.subid = subid;
+    }
+
+    public String getSubcode() {
+        return subcode;
+    }
+
+    public void setSubcode(String subcode) {
+        this.subcode = subcode;
+    }
+
+    public String getSubname() {
+        return subname;
+    }
+
+    public void setSubname(String subname) {
+        this.subname = subname;
+    }
+    
+}
